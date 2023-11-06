6a46ec8ac98dee6352535966b0d40e6523d7a2cd
arisazuhri <146687915+arisazuhri@users.noreply.github.com>
2023-11-06 23:28:20 +0700

    Add files via upload

---
+++ decafe/main.php
@@ -0,0 +1,75 @@
+<?php
+//session_start();
+if (empty($_SESSION['username_decafe'])) {
+    header('location:login');
+}
+include "proses/connect.php";
+$query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$_SESSION[username_decafe]'");
+$hasil = mysqli_fetch_array($query);
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=devaice-width, initial-scale=1">
+    <title>DECAFE - Aplikasi Pemesanan Cafe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+    <!--haeder-->
+    <?php include "header.php"; ?>
+    <!--end header-->
+
+    <div class="container-lg">
+        <div class="row">
+
+            <!--sidebar-->
+            <?php include "sidebar.php"; ?>
+            <!--end sidebar-->
+
+            <!--conten-->
+            <?php
+            include $page;
+            ?>
+            <!--end conten-->
+
+
+
+
+            <div class="fixed-bottom text-center bg-light py-2">
+                Copyright 2022 TZ
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script>
+    //digunakan untuk menanda code java script//
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+    })()
+</script>
+</body>
+
+</html>
